f5922661c2029d7a1de6c3d4c3d28727941be4ad
leoniethomas <49941230+leoniethomas@users.noreply.github.com>
2019-06-15 18:09:00 +0200

    dataframetargets

--- dataframedrivermutations.R
+++ dataframedrivermutations.R
@@ -1,9 +1,14 @@
+
+load("C:/Users/LeoTh/Documents/GitHub/project-01-group-02/CellCancerLinesafterCleanup.RDS")
+
+
 # the next task is to extract the occupancy of each gene mutation 
 
-mutationoccupancy <- as.data.frame(table(Mutation_1dataframe$Hugo_Symbol))
+#mutationoccupancy <- as.data.frame(table(Mutation_1dataframe$Hugo_Symbol))
 
 
-mutationoccupancy <- mutationoccupancy[order(mutationoccupancy$Freq, decreasing = TRUE),]
+#mutationoccupancy <- mutationoccupancy[order(mutationoccupancy$Freq, decreasing = TRUE),]
+
 
 library(tidyverse)
 library(reshape2)
@@ -30,14 +35,27 @@ countingMutations$Mutations <- factor(countingMutations$Mutations, levels = coun
 ggplot(data = countingMutations)+
   (geom_bar(mapping = aes(x = Mutations, y = Occupancy), stat = "identity"))
 
-b <- countingMutations[3500:3526,]
-b$Var1 <- factor(b$Mutations, levels = b$Mutations)
+targetmutations <- countingMutations[which(countingMutations$Occupancy >6),]
+targetmutations$Var1 <- factor(targetmutations$Mutations, levels = targetmutations$Mutations)
 
-ggplot(data = b)+
+ggplot(data = targetmutations)+
   (geom_bar(mapping = aes(x = Mutations, y = Occupancy), stat = "identity"))
 
 
-Targetmutations <- countingMutations
+
+namestargetmutations <- targetmutations$Mutations 
+
+targetexpression <- allDepMap_expression_SkinCancer[which(rownames(allDepMap_expression_SkinCancer)%in% namestargetmutations),]
+targetcopynumber <- allDepMap_copynumber_SkinCancer[which(rownames(allDepMap_copynumber_SkinCancer)%in% namestargetmutations),]
+targetkd.ceres <- allDepMap_kd.ceres_SkinCancer[which(rownames(allDepMap_kd.ceres_SkinCancer)%in% namestargetmutations),]
+targetkd.prob <- allDepMap_kd.prob_SkinCancer[which(rownames(allDepMap_kd.prob_SkinCancer)%in% namestargetmutations),]
 
 
+# notargets <- countingMutations$Mutations[which(countingMutations$Occupancy < 7)]
+
+# targetexpression2 <- allDepMap_expression_SkinCancer[-which(rownames(allDepMap_expression_SkinCancer) %in% notargets),]
+
+
+save(file= "C:/Users/LeoTh/Documents/GitHub/project-01-group-02/targetdata.RDS", list="countingMutations", "targetexpression", "targetcopynumber", "namestargetmutations", "targetkd.ceres", "targetkd.prob","Mutation_1dataframe")
+load("C:/Users/LeoTh/Documents/GitHub/project-01-group-02/targetdata.RDS")
 
