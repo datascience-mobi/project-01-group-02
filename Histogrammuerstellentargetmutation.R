59ebe2b7a0ecb8c9e0306f32c1fc7808c9dc9426
leoniethomas <49941230+leoniethomas@users.noreply.github.com>
2019-06-21 16:22:54 +0200

    Vorbereitung der Daten auf den T-test

--- Histogrammuerstellentargetmutation.R
+++ Histogrammuerstellentargetmutation.R
@@ -16,8 +16,6 @@ library(reshape2)
 
 # building a dataframe with the counts of mutated cells per mutation 
 
-
-
 countingMutations <- table(Mutation_1dataframe$Hugo_Symbol)
 countingMutations <- melt(countingMutations)
 countingMutations = countingMutations[order(countingMutations$value),]
@@ -25,17 +23,20 @@ countingMutations = countingMutations[order(countingMutations$value),]
 dim(countingMutations)
 
 rownames(countingMutations)
-rownames(countingMutations) <- c(1:3526)
+rownames(countingMutations) <- c(1:length(rownames(countingMutations)))
 
 colnames(countingMutations) <- c("Mutations", "Occupancy")
 
 
 countingMutations$Mutations <- factor(countingMutations$Mutations, levels = countingMutations$Mutations)
 
+
+#Histogramm mit der Anzahl von Mutationen für alle Gene 
 ggplot(data = countingMutations)+
   (geom_bar(mapping = aes(x = Mutations, y = Occupancy), stat = "identity"))
 
-targetmutations <- countingMutations[which(countingMutations$Occupancy >6),]
+# Histogramm mit den Genen welche mehr als 7 mal mutiert sind 
+targetmutations <- countingMutations[which(countingMutations$Occupancy >7),]
 targetmutations$Var1 <- factor(targetmutations$Mutations, levels = targetmutations$Mutations)
 
 ggplot(data = targetmutations)+
@@ -43,9 +44,19 @@ ggplot(data = targetmutations)+
 
 
 
+# extrahieren der Daten für die meistmutierten Gene und speichern in extra datenset 
+
+
 namestargetmutations <- targetmutations$Mutations 
 
-targetexpression <- allDepMap_expression_SkinCancer[which(rownames(allDepMap_expression_SkinCancer)%in% namestargetmutations),]
+
+#targetexpression <- allDepMap_expression_SkinCancer[grep("^RAS.*", as.vector(rownames(allDepMap_expression_SkinCancer))),]
+targetexpression <- allDepMap_expression_SkinCancer[grep("^RB.*", as.vector(rownames(allDepMap_expression_SkinCancer))),]
+targetexpression <- rbind(targetexpression,allDepMap_expression_SkinCancer[grep("^NF.*", as.vector(rownames(allDepMap_expression_SkinCancer))),])
+targetexpression <- rbind(targetexpression, allDepMap_expression_SkinCancer[which(rownames(allDepMap_expression_SkinCancer)%in% namestargetmutations),])
+
+namestargetmutations <- rownames(targetexpression)
+
 targetcopynumber <- allDepMap_copynumber_SkinCancer[which(rownames(allDepMap_copynumber_SkinCancer)%in% namestargetmutations),]
 targetkd.ceres <- allDepMap_kd.ceres_SkinCancer[which(rownames(allDepMap_kd.ceres_SkinCancer)%in% namestargetmutations),]
 targetkd.prob <- allDepMap_kd.prob_SkinCancer[which(rownames(allDepMap_kd.prob_SkinCancer)%in% namestargetmutations),]
--- Mainscript.R
+++ Mainscript.R
@@ -18,7 +18,7 @@ allDepMap_annotation_SkinCancer =  allDepMapData$annotation[which(allDepMapData$
 save(file= "C:/Users/LeoTh/Documents/GitHub/project-01-group-02/CellCancerLines.RDS", list="allDepMap_annotation_SkinCancer", "allDepMap_copynumber_SkinCancer", "allDepMap_expression_SkinCancer", "allDepMap_kd.ceres_SkinCancer", "allDepMap_kd.prob_SkinCancer", "allDepMap_mutation_SkinCancer")
 load("C:/Users/LeoTh/Documents/GitHub/project-01-group-02/CellCancerLines.RDS")
 rm(allDepMapData)
-      
+
 
 a = 1
 while (a < length(names(allDepMap_mutation_SkinCancer))) { 
@@ -66,6 +66,35 @@ Mutation_1dataframe = Mutation_1dataframe[order(Mutation_1dataframe$Chromosome),
 Mutation_1dataframe = Mutation_1dataframe[order(Mutation_1dataframe$Start_position),]
 
 Mutation_1dataframe = Mutation_1dataframe[order(Mutation_1dataframe$Hugo_Symbol),]
+
+Mutation_1dataframe = Mutation_1dataframe[-which(Mutation_1dataframe$isDeleterious == FALSE),]
+
+# wir wollen jtzt die gleichen Gene in eine Bezeichung zusammenfassen damit 
+# sich damit besser arbeiten lässt
+
+Mutation_1dataframe <- cbind(Mutation_1dataframe,Mutation_1dataframe$Hugo_Symbol)
+Mutation_1dataframe <- Mutation_1dataframe[, c(1,2,17,3,4,5,6,7,8,9,10,11,12,13,14,15,16)]
+colnames(Mutation_1dataframe)[3] <- "Hugo_Symbols2"
+
+#Mutation_1dataframe[grep('^ABCA.*', as.vector(Mutation_1dataframe$Hugo_Symbol), value = F),3] = "ABCA"
+
+grep('^WT.*', as.vector(Mutation_1dataframe$Hugo_Symbol), value = F)
+Mutation_1dataframe[grep("^WT.*", as.vector(Mutation_1dataframe$Hugo_Symbol)),"Hugo_Symbol"] <- "WT"
+
+grep('^RAS.*', as.vector(Mutation_1dataframe$Hugo_Symbol), value = F)
+Mutation_1dataframe[grep("^RAS.*", as.vector(Mutation_1dataframe$Hugo_Symbol)),"Hugo_Symbol"] <- "RAS"
+
+grep('^RB.*', as.vector(Mutation_1dataframe$Hugo_Symbol), value = F)
+Mutation_1dataframe[grep("^RB.*", as.vector(Mutation_1dataframe$Hugo_Symbol)),"Hugo_Symbol"] <- "RB"
+
+grep('^NF.*', as.vector(Mutation_1dataframe$Hugo_Symbol), value = F)
+Mutation_1dataframe[grep("^NF.*", as.vector(Mutation_1dataframe$Hugo_Symbol)),"Hugo_Symbol"] <- "NF"
+
+grep('^BRAF.*', as.vector(Mutation_1dataframe$Hugo_Symbol), value = F)
+Mutation_1dataframe[grep("^BRAF.*", as.vector(Mutation_1dataframe$Hugo_Symbol)),"Hugo_Symbol"] <- "BRAF"
+
+
+#Mutation_1dataframe[grep("^[A-Za-z-0-9_]+(\@)?$", as.vector(Mutation_1dataframe$Hugo_Symbol)),"Hugo_Symbol"] <- "ABCA"
 
 allDepMap_copynumber_SkinCancer = allDepMap_copynumber_SkinCancer[order(rownames(allDepMap_copynumber_SkinCancer)),]
 allDepMap_expression_SkinCancer = allDepMap_expression_SkinCancer[order(rownames(allDepMap_expression_SkinCancer)),]
